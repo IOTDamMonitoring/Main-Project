f4a909a269a4e0d955af0b0bc7c713ee712a709e
IOTDamMonitoring <49222862+IOTDamMonitoring@users.noreply.github.com>
2019-06-13 07:35:27 +0530

    update version 1.0

--- Sensors/dht-firebase.py
+++ Sensors/dht-firebase.py
@@ -14,16 +14,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 GPIO.setwarnings(False)
 
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT11
 
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
 pin = 4
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 
